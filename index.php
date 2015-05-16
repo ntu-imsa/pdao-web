c46bea1fa945e1ee4129f4e69bc9672f835ceb9f
Shouko <h999342@gmail.com>
2015-05-16 11:09:41 +0800

    Add GA

--- index.php
+++ index.php
@@ -115,9 +115,9 @@ if(ONLINE == 1){
 								refresh();
 <?php
 if(ONLINE == 1){
+	echo 'setInterval(refresh, 3000);';
 ?>
-								setInterval(refresh, 3000);
-								$('#pdao_chat').html('<iframe src="<?=CHAT_URL ?>" style="width: 100%; height: 500px"></iframe>');
+	$('#pdao_chat').html('<iframe src="<?=CHAT_URL ?>" style="width: 100%; height: 500px"></iframe>');
 <?php
 }
 ?>
@@ -233,6 +233,15 @@ if(ONLINE == 1){
 					</div>
 
 			</div>
+<script type="text/javascript">
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-45786123-2', 'auto');
+ 
+	ga('send', 'pageview');
+</script>
 
 	</body>
 </html>
